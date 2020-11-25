34e07e59a9fc3a21c0d9bc15e65c4b72968dd1f7
andrew <ajacoboc@unsa.edu.pe>
2020-11-24 20:40:52 -0500

    Terminamos las operaciones crud

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListarMoviesComponent } from './components/listar-movies/listar-movies.component';
-
+import { CrearMoviesComponent } from './components/crear-movies/crear-movies.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { UpdateComponent } from './components/update/update.component';
 const routes: Routes = [
   {
       path: '',
       component: ListarMoviesComponent,
+  },
+  {
+    path: 'create',
+    component: CrearMoviesComponent,
+  },
+  {
+    path: 'detail',
+    component: DetailComponent,
+  },
+  {
+    path: 'update',
+    component: UpdateComponent,
   }
 ];
 
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ListarMoviesComponent } from './components/listar-movies/listar-movies.component';
 import { CrearMoviesComponent } from './components/crear-movies/crear-movies.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { UpdateComponent } from './components/update/update.component';
 
 
 @NgModule({
   declarations: [
     ListarMoviesComponent,
     CrearMoviesComponent,
+    MenuComponent,
+    DetailComponent,
+    UpdateComponent,
   ],
   imports: [
     CommonModule,
